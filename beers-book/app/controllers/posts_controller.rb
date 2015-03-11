6278b185f8b3488eaeffe9ddc5d780a252131baa
nbeers22 <nbeers22@gmail.com>
2015-03-11 14:25:08 -0400

    Fixed bug with template not rendering correctly and css for post boxes

--- beers-book/app/controllers/posts_controller.rb
+++ beers-book/app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
 	def create
 		@post = Post.create(post_params)
+		@post.user = current_user if current_user
+		@post.save
 	end
 
 	def edit
@@ -27,7 +29,7 @@ class PostsController < ApplicationController
 	private
 
 	def post_params
-		params.require(:post).permit(:content)
+		params.require(:post).permit(:content, :user_id)
 	end
 
 	def all_posts
